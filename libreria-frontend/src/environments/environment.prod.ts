7320fdd161a30f69a3eb7c7deb3f48529e6d2ef0
CristianCabrera2004 <cristianalejandrocabreraestrad@gmail.com>
2026-09-02 02:46:19 -0500

    Update API URL for production environment

--- libreria-frontend/src/environments/environment.prod.ts
+++ libreria-frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@
 // App: backend-cinder-shape-4030 → https://fly.io/apps/backend-cinder-shape-4030
 export const environment = {
   production: true,
-  apiUrl: 'https://altares-api.onrender.com/api'
+  apiUrl: 'https://altares-web.onrender.com/api'
 };
